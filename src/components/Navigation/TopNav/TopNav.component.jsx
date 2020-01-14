e89584fc6586c617fc10323dfaa95ab6714bf20d
Erayus <raymondhieutran@gmail.com>
2020-01-15 00:02:01 +1100

    [New] Styling TopNav on mobile view

--- src/components/Navigation/TopNav/TopNav.component.jsx
+++ src/components/Navigation/TopNav/TopNav.component.jsx
@@ -3,24 +3,22 @@ import {
 MDBNavbar, MDBIcon, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBCollapse
 } from "mdbreact";
 import moment from "moment";
-
+import classes from './TopNav.module.css';
 const  topNav = (props) => {
 
   return (
       <MDBNavbar color="elegant-color-dark" dark expand="md" className="mb-5">
         
-        <MDBCollapse id="navbarCollapse3" navbar>
           <MDBNavbarNav>
                 <MDBNavbarBrand>
                 <strong className="white-text pl-5" style={{fontSize: '24px'}}>Yarra Traffic Report Generator</strong>
             </MDBNavbarBrand>
           </MDBNavbarNav>
-          <MDBNavbarNav right>
-            <MDBNavItem style={{color: 'white', background: '#4B515D', width: '150px', padding: '10px 0px'}}>
+          <MDBNavbarNav right className={classes.RightNav}>
+            <MDBNavItem  style={{color: 'white', background: '#4B515D', width: '150px', padding: '10px 0px'}}>
                 <MDBIcon far icon="calendar-alt" className="mr-1" /> {moment().format("MMM Do YY")}
             </MDBNavItem>
           </MDBNavbarNav>
-        </MDBCollapse>
       </MDBNavbar>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 
